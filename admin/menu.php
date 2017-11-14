359057148055e856135ae858a42aac91c0f6327c
Trankimacin <33525176+Trankimacin@users.noreply.github.com>
2017-11-14 13:05:37 +0100

    - Cambio en el menú - Añadido el CSS para el mismo

---
+++ admin/menu.php
@@ -0,0 +1,32 @@
+<meta charset="utf-8">
+<link rel="stylesheet" type="text/css" href="../css/estilos.css">
+
+<nav id='menu'>
+	<input type='checkbox'/>
+	<label>&#8801;<span>Navigation</span></label>
+	<ul>
+		<li><a class='prett' href='#'>Revistas</a>
+			<ul class='menus'>
+				<li><a href='altaRevista.php'>Alta Revista</a></li>
+				<li><a href='modificarRevista.php'>Modificar Revista</a></li>
+				<li><a href='bajaRevista.php'>Eliminar Revista</a></li>
+			</ul>
+		</li>
+		<li><a class='prett' href='#'>Articulos</a>
+			<ul class='menus'>
+				<li><a href='altaArticulos.php'>Alta Articulo</a></li>
+				<li><a href='modificarArticulos.php'>Modificar Articulo</a></li>
+				<li><a href='bajaUsuario.php'>Eliminar Articulo</a></li>
+			</ul>
+		</li>
+		
+		<li><a class='prett' href='#'>Usuarios</a>
+			<ul class='menus'>
+				<li><a href='altaUsuario.php'>Alta Usuario</a></li>
+				<li><a href='modificarUsuario.php'>Modificar Usuario</a></li>
+				<li><a href='bajaUsuario.php'>Eliminar Usuario</a></li>
+			</ul>
+		</li>
+		<li><a href='salir.php'>Desconectarse</a></li>
+	</ul>
+</nav>
