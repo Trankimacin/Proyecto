770b4181e7507ed121baa70e5eca021bfd498d16
Trankimacin <trankimacin@MacBook-Pro-de-Trankimacin.local>
2017-11-15 12:24:49 +0100

    Añadido código necesario para la modificación de revistas

--- admin/modificarRevista.php
+++ admin/modificarRevista.php
@@ -6,7 +6,57 @@
 	require_once("conexion.php");
 
 	if(isset($_POST['cod_revista'])){
+		if(empty($_FILES['archivo']['name'])){
 
+			$cod_revista = $_POST['cod_revista'];
+			$numero      = $_POST['numero'];
+			$fecha       = $_POST['fecha'];
+			$portada     = $_POST['portada'];
+			$publicada   = $_POST['publicada'];
+
+			$consulta = "UPDATE revistas SET numero='$numero', fecha='$fecha', portada='$portada', publicada=$publicada WHERE cod_revista='$cod_revista';";
+			mysqli_query($conexion, $consulta);
+
+			if(mysqli_errno($conexion)==0){
+				echo ("<h2>Revista actualizada correctamente</h2>");
+			}else{
+				echo ("<h2>La revista no se pudo actualizar</h2>");
+			}
+
+		}else{
+			
+			$info = getimagesize($_FILES['archivo']['tmp_name']);
+
+			if (($info[2] !== IMAGETYPE_GIF) && ($info[2] !== IMAGETYPE_JPEG) && ($info[2] !== IMAGETYPE_PNG)) {
+  				echo ("<h2>Solo se admiten lo siguientes archivos: .gif / .jpeg / .png");
+  			}else{
+
+				$archivo = $_FILES['archivo']['tmp_name'];
+				$destino = '../media/img/portadas/' .$_FILES['archivo']['name'];
+				$nombre = $_FILES['archivo']['name'];
+
+				if(move_uploaded_file($archivo, $destino)){
+					$cod_revista = $_POST['cod_revista'];
+					$numero      = $_POST['numero'];
+					$fecha       = $_POST['fecha'];
+					$publicada   = $_POST['publicada'];
+
+					$consulta = "UPDATE revistas SET numero='$numero', fecha='$fecha', portada='$nombre', publicada=$publicada WHERE cod_revista='$cod_revista';";
+
+					mysqli_query($conexion, $consulta);
+
+					if(mysqli_errno($conexion)==0){
+						echo ("<h2>Revista actualizada correctamente</h2>");
+					}else {
+						echo ("<h2>La revista no se pudo actualizar</h2>");
+					}
+
+				}else{
+					echo ("<h2>No se pudo hacer la modificación</h2>");
+				}
+
+  			}
+		}
 
 	}
 
@@ -23,7 +73,7 @@
 			echo ("
 				<h2>Modifica la revista</h2>
 
-				<form name='formulario' action='modificarRevista.php' method='post'>
+				<form name='formulario' action='modificarRevista.php' method='post' enctype='multipart/form-data'>
 			");
 
 			while($dato=mysqli_fetch_array($resultado)){
@@ -33,9 +83,9 @@
 					<p><label>Número de la revista</label>
 					<input type='number' name='numero' value='".$dato['numero']."'></p>
 					<p><label>Fecha: </label>
-					<input type='text name='fecha' value='".$dato['fecha']."'></p>
+					<input type='text' name='fecha' value='".$dato['fecha']."'></p>
 					<p><label>Portada: </label>
-					<input type='text' name='portada' value='".$dato['portada']."' disabled></p>
+					<input type='text' name='portada' value='".$dato['portada']."' readonly></p>
 					<p><label>Nueva portada</label>
 					<input type='file' name='archivo' style='color: transparent;'></p>
 					<p><label>Públicar</label></p>
