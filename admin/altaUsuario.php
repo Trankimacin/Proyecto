92d6a8b2a885fcc363856f9eeb0b21c12699d337
Trankimacin <trankimacin@MacBook-Pro-de-Trankimacin.local>
2017-11-16 12:48:20 +0100

    Puesto placeholder en los campos de contraseña

--- admin/altaUsuario.php
+++ admin/altaUsuario.php
@@ -28,8 +28,27 @@
   <h2 class="login-header">Registrar nuevo usuario</h2>
 
   <form name="formulario" action="altaUsuario.php" method="post" onsubmit="validar(event)">
-    <p><input type="text" placeholder="Usuario" name="usuario"></p>
-    <p><input type="password" id="pass_1" placeholder="Password" name="pass_1"></p>
-    <p><input type="password" id="pass_2" placeholder="Password" name="pass_2"></p>
+	<div class="bloque">
+	  	<input type="text" placeholder="Usuario" name="usuario">
+	    	<div class="tooltip">
+	    		<img src="../media/icon/info.png">
+	    			<span class="tooltiptext">Introduce un correo</span>
+	    	</div>
+    </div>
+    <div class="bloque">
+	    <input type="password" id="pass_1" placeholder="Password" name="pass_1">
+	    	<div class="tooltip">
+	    		<img src="../media/icon/info.png">
+	    			<span class="tooltiptext">Introduce una contraseña</span>
+	    	</div>
+    </div>
+    <div class="bloque">
+		<input type="password" id="pass_2" placeholder="Password" name="pass_2">
+			<div class="tooltip">
+				<img src="../media/icon/info.png">
+					<span class="tooltiptext">Repite contraseña</span>
+			</div>
+	</div>
+	</div>
     <p><input type="submit" value="Registrar"></p>
   </form>
