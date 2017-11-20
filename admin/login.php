d1ac6033c1510feb04d1eb01fc84c149a8a3b7b8
Trankimacin <33525176+Trankimacin@users.noreply.github.com>
2017-11-20 20:26:46 +0100

    CSS
    Cambios en el CSS para que sea formulario de bootstrap.

--- admin/login.php
+++ admin/login.php
@@ -2,7 +2,9 @@
 <html>
 <head>
 	<meta charset="utf-8">
-	<link rel="stylesheet" href="../css/estilos.css">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 	<title></title>
 </head>
 <body>
@@ -25,17 +27,36 @@
 	?>
 
 
-<form action="validacion.php" method="post">
-  <div class="container">
-    <label>Usuario</label>
-    <input type="text" placeholder="Enter Username" name="usuario_txt" required>
 
-    <label>Password</label>
-    <input type="password" placeholder="Enter Password" name="pass_txt" required>
-        
-    <input type="submit" name="enviar_btn" value="Login">
-  </div>
-</form>
+  <div class="modal-dialog">
+    <div class="modal-content">
+
+      <div class="modal-header">
+        <h4 class="modal-title" id="myModalLabel">Log in</h4>
+      </div> <!-- /.modal-header -->
+
+      <div class="modal-body">
+        <form role="form" method="post" action="validacion.php">
+          <div class="form-group">
+            <div class="input-group">
+              <input type="text" name="usuario_txt" class="form-control" id="uLogin" placeholder="Login">
+              <label for="uLogin" class="input-group-addon glyphicon glyphicon-user"></label>
+            </div>
+          </div> <!-- /.form-group -->
+
+          <div class="form-group">
+            <div class="input-group">
+              <input type="password" name="pass_txt" class="form-control" id="uPassword" placeholder="Password">
+              <label for="uPassword" class="input-group-addon glyphicon glyphicon-lock"></label>
+            </div> <!-- /.input-group -->
+
+          </div> <!-- /.form-group -->
+          <button name="enviar_btn" class="form-control btn btn-primary">Log-in</button>
+        </form>
+
+      </div> <!-- /.modal-body -->
+    </div><!-- /.modal-content -->
+  </div><!-- /.modal-dialog -->
 
 </body>
 </html>
