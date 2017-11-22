7c501c356a6ebe7877e200a47ee45282068ef516
Trankimacin <trankimacin@MacBook-Pro-de-Trankimacin.local>
2017-11-22 01:01:12 +0100

    Borrado el CSS del footer y cambios en los mensajes de abajo.

--- footer.php
+++ footer.php
@@ -1,47 +1,13 @@
-<!-- CSS -->
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-
-<!-- JQuery -->
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-
-<!-- JavaScript -->
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-
-<style>
-
-.prueba {
-    background-color:#2b2b2b;
-    color:#ddd;
-    min-height:100px;
-    padding:20px 0;
-}
-
-.prueba .nav>li>a {
-    padding: 3px;
-    color: #f44d3c;
-}
-.prueba .nav>li{
-    padding: 3px;
-    color: #f44d3c;
-}
-
-.prueba .nav>li>a:hover {
-    background-color:transparent;
-    color:#fff;
-}
-
-</style>
-
-<nav class="prueba navbar navbar-fixed-botton " role=navigation">
+<nav class="prueba navbar navbar-fixed-botton" role="navigation">
     <div class="container">
         <div class="row">
-            <div class="col-md-6 column">
+            <div class="col-md-4 col-md-offset-2">
                 <ul class="nav">
                     <li><a href="about-us.html">Politica de privacidad</a></li>
                     <li><a href="">Mapa de la web</a></li>
                 </ul>
             </div>
-            <div class="col-xs-6 col-md-6 column">
+            <div class="col-md-4 col-md-offset-2">
                 <ul class="nav">
                     <li>Direccion</li>
                     <li>C.I.F</li>
