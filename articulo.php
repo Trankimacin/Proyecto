1c6328941d3561ba11bd0731d1fb0142dd269a4b
Trankimacin <trankimacin@MacBook-Pro-de-Trankimacin.local>
2017-11-22 17:46:56 +0100

    Añadido el principio del código.

--- articulo.php
+++ articulo.php
@@ -1,11 +1,12 @@
 <?php
 
-	require_once('conexion.php');
-	
-	if(isset($_GET['c'])){
-		$cod_articulo = $_GET['c'];
-	}
+	include_once("menu.php");
 
-
+	echo ("
+		<div class='container'>
+			<div class='row'>
+				<div class='col-md-9'>
+					<h2 class='text-center text-danger'>Listado de articulos</h2>
+	");
 
 ?>
