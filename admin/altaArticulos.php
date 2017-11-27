1e3fd9dd26d2cf5e6b8019c749db61fda074d3a0
Trankimacin <33525176+Trankimacin@users.noreply.github.com>
2017-11-27 17:56:24 +0100

    Borrado
    Quitado lo botones de "Borrar"

--- admin/altaArticulos.php
+++ admin/altaArticulos.php
@@ -181,7 +181,6 @@
 			<label class="col-md-4 control-label"></label>
 			<div class="col-md-8">
 				<button class="btn btn-success">A&ntilde;adir</button>
-				<button class="btn btn-danger">Borrar</button>
 			</div>
 		</div>
 	</fieldset>
