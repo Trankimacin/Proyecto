e7cb173a7c3b074754ded3dfae411fc673a39e8c
Trankimacin <33525176+Trankimacin@users.noreply.github.com>
2017-11-29 20:06:04 +0100

    Borrarte a ti mismo
    - Quitamos la opción de borrarte a ti mismo, y que no aparezca en la lista tampoco para evitar confusiones.
    - Añadido un tooltip, para avisar al usuario que nunca se podrá borrar a si mismo.

--- admin/bajaUsuario.php
+++ admin/bajaUsuario.php
@@ -7,51 +7,32 @@
 
 	if(isset($_POST['desplegable'])){
 
-		$cod_usuario = $_POST['desplegable'];
+		$consulta2 = "DELETE FROM usuarios WHERE cod_usuario=$cod_usuario;";
 
-		$usuario     = $_SESSION['usuario'];
-		
-		$consulta    = "SELECT cod_usuario FROM usuarios WHERE usuario='$usuario';";
-		
-		$resultado   = mysqli_query($conexion, $consulta);
+		$resultado2 = mysqli_query($conexion, $consulta2);
 
-		$dato = mysqli_fetch_assoc($resultado);
-
-		if($dato['cod_usuario']==$cod_usuario){
+		if(mysqli_errno($conexion)==0){
 			echo ("
-				<div class='alert alert-warning fade in'>
+				<div class='alert alert-success fade in'>
 					<a href='' class='close' data-dismiss='alert'>&times;</a>
-					<span class='glyphicon glyphicon-info-sign' aria-hidden='true'></span>
-					No puedes borrarte a ti mismo
+					<span class='glyphicon glyphicon-ok' aria-hidden='true'></span>
+					Se ha eliminado correctamente
 				</div>
 			");
 		}else{
-
-			$consulta2 = "DELETE FROM usuarios WHERE cod_usuario=$cod_usuario;";
-
-			$resultado2 = mysqli_query($conexion, $consulta2);
-
-			if(mysqli_errno($conexion)==0){
-				echo ("
-					<div class='alert alert-success fade in'>
-						<a href='' class='close' data-dismiss='alert'>&times;</a>
-						<span class='glyphicon glyphicon-ok' aria-hidden='true'></span>
-						Se ha eliminado correctamente
-					</div>
-				");
-			}else{
-				echo ("
-					<div class='alert alert-danger fade in'>
-						<a href='' class='close' data-dismiss='alert'>&times;</a>
-						<span class='glyphicon glyphicon-remove' aria-hidden='true'></span>
-						No se ha podido eliminar
-					</div>
-				");
-			}
+			echo ("
+				<div class='alert alert-danger fade in'>
+					<a href='' class='close' data-dismiss='alert'>&times;</a>
+					<span class='glyphicon glyphicon-remove' aria-hidden='true'></span>
+					No se ha podido eliminar
+				</div>
+			");
 		}
 	}
 
-	$consulta = "SELECT * FROM usuarios ORDER BY usuario;";
+	$usuario = $_SESSION['usuario'];
+
+	$consulta = "SELECT * FROM usuarios WHERE usuario != '$usuario' ORDER BY usuario;";
 
 	$resultado = mysqli_query($conexion, $consulta);
 
@@ -60,7 +41,7 @@
 		<div class='modal-dialog'>
 			<div class='modal-content'>
 				<div class='modal-header'>
-					<h4 class='modal-title'>Eliminar un usuario</h4>
+					<h4 class='modal-title'>Eliminar un usuario <a href='#' data-trigger='focus' data-toggle='borrar' data-content='Nunca podrás borrarte a ti mismo' class='glyphicon glyphicon-cog'></a></h4>
 				</div>
 				<div class='modal-body'>
 					<form action='bajaUsuario.php' method='post' onsubmit='return seleccionado();'>
@@ -87,3 +68,9 @@
 	");
 
 ?>
+
+<script>
+$(document).ready(function(){
+    $('[data-toggle="borrar"]').popover();
+});
+</script>
